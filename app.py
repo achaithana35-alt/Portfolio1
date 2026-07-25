9b4f8e73bc02c6b756ebb707469f21004e83eba5
achaithana35-alt <achaithana35@gmail.com>
2026-07-25 09:44:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
-
+app.config["MAIL_MAX_EMAILS"] = 1
+app.config["MAIL_ASCII_ATTACHMENTS"] = False
+app.config["MAIL_TIMEOUT"] = 10
 # Environment Variables (Set these in Render)
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
